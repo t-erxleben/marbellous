7a4a8332e23104f5f2362feb41641c6f4895b2ff
Timm Erxleben <timm.erxleben@ovgu.de>
2021-05-01 16:53:22 +0200

    add first simple version of options singleton

---
+++ demo/backend/include/Options.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Palette.hpp"
+
+class Options
+{
+    private:
+        // realize singleton pattern
+        static Options* instance;
+        Options();
+
+
+        // private data members
+        Palette activePalette{};
+        Color bgColor{0xFFFFFF};
+
+    public:
+        // singleton should neither be copied nor assigned
+        Options(Options const &) = delete;
+        void operator=(Options const &) = delete;
+        // access instance
+        Options * const getInstance();
+
+        void setPalette(Palette const & newPalette);
+        Palette* getPalette();
+        void setBGColor(Color const & col);
+        Color* getBGColor();
+
+
+};
