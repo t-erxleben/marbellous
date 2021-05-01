85188651a44c20981a28edd3b869db02b3a7f12b
Julian Benda <julian.benda@ovgu.de>
2021-05-01 18:44:51 +0200

    Add state transition

--- WebFrontend/actions.js
+++ WebFrontend/actions.js
@@ -1,4 +1,8 @@
 var active = {};
+const states = ['draw','rake'];
+var state = 'draw';
+var nodes = {};
+
 function getTopSubmenu(label) {
 	var el = label;
 	var menu;
@@ -19,6 +23,37 @@ function getTopSubmenu(label) {
 	return menu;
 }
 
+function switchState(_old, _new) {
+	nodes[_old] = []
+	Array.prototype.forEach.call(document.getElementsByClassName('state ' + _old), function (e){
+		var next = e;
+		var hit = false;
+		do{
+			next = next.nextSibling;
+			while(next && next.tagName != 'LI') { next = next.nextSibling; }
+			hit = false;
+			if(next) {
+				states.forEach(function(s){
+					if (next.classList.contains(s)) { hit = true; }
+				});
+			}
+			console.log(hit, next);
+		}while(hit);
+		nodes[_old].push({node: e, par: e.parentNode, next});
+	});
+	if(nodes[_new]) {
+		nodes[_new].forEach(function(e){
+			e.par.insertBefore(e.node, e.next);
+		});
+	}
+	nodes[_old].forEach(function(e){
+		e.node.remove();
+	});
+
+}
+
+
+
 document.addEventListener("click", function(evnt){
 	var el = evnt.target;
 	// hide submenu if element is double clicked, show it again when clicked again or first selected
@@ -36,6 +71,22 @@ document.addEventListener("click", function(evnt){
 			if (menu) {
 				menu.querySelectorAll('li').forEach(function(li){li.classList.remove('hide')});
 			}
+		}
+	}
+	else if (el.tagName == 'INPUT'
+		&& el.attributes.type
+		&& el.attributes.type.nodeValue == 'button')
+	{
+		switch(el.id) {
+			case 'switch-state':
+				console.log('switch');
+				const img = el.parentNode.querySelector('img');
+				const oldState = state;
+				switch(state) {
+					case 'draw': state = 'rake'; img.src = 'icons/image-regular.svg'; break;
+					case 'rake': state = 'draw'; img.src = 'icons/arrow-right-solid.svg'; break;
+				}
+				switchState(oldState, state);
 		}
 	}
 });
@@ -61,4 +112,8 @@ document.addEventListener("DOMContentLoaded", function(){
 			}
 		}
 	});
+	states.forEach(function(s){
+		switchState(s, null);
+	});
+	switchState(null, state);
 });
