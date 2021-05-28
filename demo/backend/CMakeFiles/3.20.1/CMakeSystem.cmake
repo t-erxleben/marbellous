10c44f65123aef98df1d83cb7ae03483b20920b2
t-erxleben <73595753+t-erxleben@users.noreply.github.com>
2021-05-28 12:00:42 +0000

    Deploying to gh-pages from @ t-erxleben/marbellous@bd0d6bb3c6d79e36977e3ad3a98d5e3444713bc2 🚀

--- demo/backend/CMakeFiles/3.20.1/CMakeSystem.cmake
+++ demo/backend/CMakeFiles/3.20.1/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-1047-azure")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/runner/work/_temp/dd0c560c-34ae-40b2-8a85-3e0b9f964fec/emsdk-master/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+include("/home/runner/work/_temp/91618bae-14fd-4786-b7db-c08df8e46638/emsdk-master/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
 set(CMAKE_SYSTEM "Emscripten-1")
 set(CMAKE_SYSTEM_NAME "Emscripten")
--- demo/backend/include/Color.hpp
+++ demo/backend/include/Color.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <tuple>
+#include <cstdint>
 
 class Color
 {
