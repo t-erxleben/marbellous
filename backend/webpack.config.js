1e0fd14323ac5feadf2576a2e2ce0ce1ae55deab
JBenda <Zabuza98@gmx.net>
2021-09-09 09:13:00 +0000

    Deploying to gh-pages from @ t-erxleben/marbellous@ef3182de7ba54f47ca6a0daa0948770078b61343 🚀

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 			}
 		]
 	},
-	mode: "development",
+	mode: "production",
 	entry: {
 		index: "/home/runner/work/marbellous/marbellous/backend/../frontend//src/index.js",
 	},
